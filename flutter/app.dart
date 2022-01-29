00c4e9bf5fa333bb270230024d89f1c2c333d086
Eduardo Pereira <30639462+eduardo-aop@users.noreply.github.com>
2022-01-29 11:17:54 -0300

    Update app.dart

--- flutter/app.dart
+++ flutter/app.dart
@@ -9,11 +9,9 @@ class ExampleScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Text('gh_owner: Owner of the Github Account. In thise case is is: Jose Alba '),
+            Text('gh_owner: Owner of the Github Account. In thise case is is: Eduardo '),
             Text('gh_repo: Name of the repo within the above account. In this case it is: flutter_code'),
             Text('gh_path: Path to a dartpad_metadata.yaml file within the repo. In this case it is: lib/dartpad'),
-            Text('Collectivaly to make a dartpad with this repository the URL would be: '),
-            Text('dartpad.dev/embed-flutter.html?gh_owner=JoseAlba&gh_repo=flutter_code&gh_path=lib/dartpad'),
           ],
         ),
       ),
