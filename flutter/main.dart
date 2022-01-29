d4f3c19f494539c8f9e0932dc4f9efb1c7ba55aa
Eduardo Pereira <30639462+eduardo-aop@users.noreply.github.com>
2022-01-29 11:24:28 -0300

    Update main.dart

--- flutter/main.dart
+++ flutter/main.dart
@@ -2,19 +2,6 @@ import 'package:flutter/material.dart';
 class ExampleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(' Creating a Dartpad with a repository!'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text('gh_owner: Owner of the Github Account. In thise case is is: Eduardo '),
-            Text('gh_repo: Name of the repo within the above account. In this case it is: flutter_code'),
-            Text('gh_path: Path to a dartpad_metadata.yaml file within the repo. In this case it is: lib/dartpad'),
-          ],
-        ),
-      ),
-    );
+    return Text('gh_owner: Owner of the Github Account. In thise case is is: Eduardo ');
   }
 }
